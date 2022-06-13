c53d1b85b11e770a797b896989c1507ff7703e4f
HustleCoding <f.dobinciuc7@gmail.com>
2022-06-14 00:40:56 +0300

    Create the secret number and write some game logic

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", () => {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🙌 Correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too high!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too low!";
+  }
+});
