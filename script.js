ca4253c877ab1cd70d95bbde01ee5d995647be7f
HustleCoding <f.dobinciuc7@gmail.com>
2022-06-29 20:53:39 +0300

    Start to implement game logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", () => {
@@ -12,7 +13,9 @@ document.querySelector(".check").addEventListener("click", () => {
     document.querySelector(".message").textContent = "🙌 Correct Number!";
   } else if (guess > secretNumber) {
     document.querySelector(".message").textContent = "Too high!";
+    score--;
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "Too low!";
+    score--;
   }
 });
